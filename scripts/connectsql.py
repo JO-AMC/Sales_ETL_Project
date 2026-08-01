17ccf1aeb8930907f8c67ebad611f5779af38acd
John Steephen Raj <johnsteepheh@gmail.com>
2026-08-01 16:45:48 +0530

    Update connectsql.py

--- scripts/connectsql.py
+++ scripts/connectsql.py
@@ -8,15 +8,6 @@ df = pd.read_csv("C:\\Users\\God\\Desktop\\Sales_ETL_Project\\data\\cleaned\\Cle
 engine = create_engine(
     "mysql+pymysql://root:John%402007@localhost:3306/sales_etl"
 )
-#change column names
-df.columns = [
-    "Order_ID",
-    "Product",
-    "Quantity_Ordered",
-    "Price_Each",
-    "Order_Date",
-    "Purchase_Address"
-]
 
 #Load data
 df.to_sql("Sales", con=engine, if_exists="append", index = False)
